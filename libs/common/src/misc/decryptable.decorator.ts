4ac5152453ea3032bc368d744b8d2d4689a490fa
Thomas Rittson <trittson@bitwarden.com>
2022-09-26 16:46:33 +1000

    Add comments

--- libs/common/src/misc/decryptable.decorator.ts
+++ libs/common/src/misc/decryptable.decorator.ts
@@ -1,5 +1,8 @@
 import "reflect-metadata"; // TODO: this can only be imported in one place, so shouldn't be here
 
+/**
+ * Returns a decorator which will add the decorated property name to an array, which is stored under the metadataKey
+ */
 function listDecoratorFactory(metadataKey: symbol) {
   return function (prototype: any, propertyKey: string) {
     const encStringList: string[] = Reflect.getMetadata(metadataKey, prototype);
@@ -11,6 +14,9 @@ function listDecoratorFactory(metadataKey: symbol) {
   };
 }
 
+/**
+ * Returns a function which returns an array of property names stored with the list decorator under the metadataKey
+ */
 function getListFactory(metadataKey: symbol) {
   return function (target: any): string[] {
     return Reflect.getMetadata(metadataKey, target);
